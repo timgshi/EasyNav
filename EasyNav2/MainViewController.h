8561dccf3beb245be19261896ddc3a343df7afaa
tshi2 <tshi2@tshi2M1.local>
2011-08-10 23:07:44 -0700

    Initial Commit

---
+++ EasyNav2/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  EasyNav2
+//
+//  Created by Tim Shi on 8/10/11.
+//  Copyright 2011 www.timshi.com. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
