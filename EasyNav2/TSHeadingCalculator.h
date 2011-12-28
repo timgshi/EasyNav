963215bff9492dc5c17f0463293a6d3727c9351d
Tim Shi <timgshi@gmail.com>
2011-12-28 00:11:21 -0800

    renamed folders

---
+++ EasyNav2/TSHeadingCalculator.h
@@ -0,0 +1,13 @@
+//
+//  TSHeadingCalculator.h
+//  EasyNav2
+//
+//  Created by Tim Shi on 11/2/11.
+//  Copyright (c) 2011 www.timshi.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSHeadingCalculator : NSObject
+
+@end
---
+++ EasyNav2/TSHeadingCalculator.m
@@ -0,0 +1,13 @@
+//
+//  TSHeadingCalculator.m
+//  EasyNav2
+//
+//  Created by Tim Shi on 11/2/11.
+//  Copyright (c) 2011 www.timshi.com. All rights reserved.
+//
+
+#import "TSHeadingCalculator.h"
+
+@implementation TSHeadingCalculator
+
+@end
