f7661ed8479238f20f9b75e1501282f8e39bf63d
Tim Shi <timgshi@gmail.com>
2011-12-28 18:25:12 -0800

    halting tests for now

---
+++ ENTestController.m
@@ -0,0 +1,13 @@
+//
+//  ENTestController.m
+//  EasyNav
+//
+//  Created by Tim Shi on 12/28/11.
+//  Copyright (c) 2011 www.timshi.com. All rights reserved.
+//
+
+#import "ENTestController.h"
+
+@implementation ENTestController
+
+@end
