b61f5f1a2e2dc7c2b20e74128ea5f1f6d1cca55b
Tim Shi <timgshi@gmail.com>
2011-12-28 21:11:42 -0800

    KIF Framework fully functional

---
+++ KIFTestScenario+ENAdditions.h
@@ -0,0 +1,13 @@
+//
+//  KIFTestScenario+ENAdditions.h
+//  EasyNav
+//
+//  Created by Tim Shi on 12/28/11.
+//  Copyright (c) 2011 www.timshi.com. All rights reserved.
+//
+
+#import "KIFTestScenario.h"
+
+@interface KIFTestScenario (ENAdditions)
+
+@end
